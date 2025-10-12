978df74f4801ced42c4a5e13cb767e13809174fc
Yash Patkar <yashpatkar4249@gmail.com>
2025-10-13 00:58:50 +0530

    login fixed

--- backend/LMS/users/views.py
+++ backend/LMS/users/views.py
@@ -4,15 +4,17 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
     permission_classes = [AllowAny]
 
+
 class LoginView(generics.GenericAPIView):
     permission_classes = [AllowAny]
+    serializer_class = LoginSerializer
 
     def post(self, request):
         username = request.data.get("username")
